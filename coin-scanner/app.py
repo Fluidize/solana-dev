f7ff624875ce85a0516f2d15643564a6412d34ae
Fluidize <mbxiao1@gmail.com>
2025-02-11 19:44:35 -0600

    RICH UI
    added single analyzing mode for analyze_token_security. changed from using custom escape codes to py rich escape codes

--- coin-scanner/app.py
+++ coin-scanner/app.py
@@ -11,7 +11,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from tabulate import tabulate
+from rich.console import Console
+from rich.prompt import Prompt
+from rich.table import Table
+from rich.panel import Panel
+from rich.text import Text
+from rich.box import SQUARE
+
+import sys
 
 from textual.app import App
 from textual.widgets import Header, Footer, Button
@@ -69,31 +76,37 @@ class CoinData:
         
         driver = webdriver.Chrome(options=options)
         driver.execute_script("""Object.defineProperty(navigator, 'webdriver', { get: () => false });""")
-
-        token_count = len(tokens)
-
-        for token_address in tokens:
-            print(bcolors.OKBLUE + f"{token_count} left to scan." + bcolors.DEFAULT, end = '\r')
+        if type(tokens) == str: #scan one okay; tokens=1 token addr
+            check_url = f"https://jup.ag/tokens/{tokens}"
+            driver.get(check_url) #load url
+            html_content = driver.page_source
+            token_name = WebDriverWait(driver, 5).until(
+                EC.visibility_of_element_located((By.CSS_SELECTOR, ".flex.items-center.gap-1\\.5.text-xl.font-semibold"))
+            ).text #token name
+            organic_score = WebDriverWait(driver, 5).until(
+                EC.visibility_of_element_located((By.CSS_SELECTOR, ".flex.items-center.rounded-r.px-2.text-sm.font-medium.text-black"))
+            ).text #jup organic score
             
-            check_url = f"https://jup.ag/tokens/{token_address}"
-            try:
+            #organic_scores[tokens] = (token_name,float(organic_score))
+            return (token_name, tokens, float(organic_score), "https://rugcheck.xyz/tokens/"+tokens)
+
+        else:
+            token_count = len(tokens)
+
+            for token_address in tokens:
+                print(bcolors.OKBLUE + f"{token_count} coin(s) left to scan." + bcolors.DEFAULT, end = '\r')
+                check_url = f"https://jup.ag/tokens/{token_address}"
                 driver.get(check_url) #load url
-                
                 html_content = driver.page_source
-                organic_score = WebDriverWait(driver, 3).until(
-                    EC.visibility_of_element_located((By.CSS_SELECTOR, ".flex.items-center.rounded-r.px-2.text-sm.font-medium.text-black"))
-                ).text #jup organic score
-                token_name = WebDriverWait(driver, 3).until(
+                token_name = WebDriverWait(driver, 5).until(
                     EC.visibility_of_element_located((By.CSS_SELECTOR, ".flex.items-center.gap-1\\.5.text-xl.font-semibold"))
                 ).text #token name
-
-                organic_scores[token_address] = (token_name,organic_score)
-
+                organic_score = WebDriverWait(driver, 5).until(
+                    EC.visibility_of_element_located((By.CSS_SELECTOR, ".flex.items-center.rounded-r.px-2.text-sm.font-medium.text-black"))
+                ).text #jup organic score
+                organic_scores[token_address] = (token_name,float(organic_score))
                 token_count -= 1
-
-            except requests.RequestException as e:
-                print(f"An error occurred while fetching data: {e}")
-                return None
+                print("timeout")
         
         os.system('cls')
         driver.quit()
@@ -105,27 +118,12 @@ class CoinData:
         organic_scores = self.analyze_token_security(data.keys()) #addr + score
 
         #make it chart-readable
-        #columns --- token addr + token name + organic score + rugcheck link
+        #columns --- token name + token addr  + organic score + rugcheck link
         table_data = []
         for coin in organic_scores:
-            float_score = float(organic_scores[coin][1])
-            if float_score >= 80.00:
-                colored_score = bcolors.OKGREEN + organic_scores[coin][1] + bcolors.DEFAULT
-            elif float_score > 0.00:
-                colored_score = bcolors.WARNING + organic_scores[coin][1] + bcolors.DEFAULT
-            else:
-                colored_score = bcolors.FAIL + organic_scores[coin][1] + bcolors.DEFAULT
-
-            table_data.append((coin, organic_scores[coin][0], colored_score, "https://rugcheck.xyz/tokens/"+coin))
+            table_data.append((organic_scores[coin][0], coin, organic_scores[coin][1], "https://rugcheck.xyz/tokens/"+coin))
 
         return table_data
-    
-from rich.console import Console
-from rich.prompt import Prompt
-from rich.table import Table
-from rich.panel import Panel
-from rich.text import Text
-import sys
 
 # Initialize the console for output
 Console = Console()
@@ -137,13 +135,15 @@ class CommandUI:
             "help": self.show_help,
             "add": self.add_numbers,
             "scan": self.scan,
+            "scan_auto": self.scan_auto,
             "exit": self.exit_app
         }
 
     def show_help(self):
         help_text = Text("Available Commands:", style="bold green")
         help_text.append("\n  - help: Show this help message")
-        help_text.append("\n  - scan: Scan on Dexscreener.")
+        help_text.append("\n  - scan_auto: Scan coins from Dexscreener.")
+        help_text.append("\n  - scan: Scan coins from an address.")
         help_text.append("\n  - add <num1> <num2>: Add two numbers")
         help_text.append("\n  - exit: Exit the app")
         # Create a panel with the help text (no soft_wrap here)
@@ -155,8 +155,46 @@ class CommandUI:
         result = int(num1) + int(num2)
         Console.print(f"The result of adding {num1} and {num2} is {result}", style="bold yellow")
 
+    def scan_auto(self):
+        table = Table(title="[bold cyan]Jupiter Organic Scores[/bold cyan]", header_style="bold white", box=SQUARE)
+        table_data = self.CoinData.scan()
+        table.add_column("Name",justify="center", style="bold white")
+        table.add_column("Token",justify="center")
+        table.add_column("Score",justify="center")
+        table.add_column("Rugcheck.xyz",justify="center", style="underline bright_blue")
+        for data in table_data:
+            #data[2] is score
+            if data[2] >= 80.00:
+                score_style = "bold bright_green"
+            elif data[2] > 0.00:
+                score_style = "bold bright_yellow"
+            else:
+                score_style = "bold bright_red"
+            
+            table.add_row(data[0],data[1],f"[{score_style}]{str(data[2])}[/{score_style}]", data[3])
+        Console.print(table)
+    
     def scan(self):
-        self.CoinData.scan()
+        tokens = Prompt.ask("Enter token address")
+        table_data = self.CoinData.analyze_token_security(tokens)
+
+        table = Table(title="[bold cyan]Jupiter Organic Scores[/bold cyan]", header_style="bold white", box=SQUARE)
+        table.add_column("Name",justify="center", style="bold white")
+        table.add_column("Token",justify="center")
+        table.add_column("Score",justify="center")
+        table.add_column("Rugcheck.xyz",justify="center", style="underline bright_blue")
+        
+        print(table_data)
+
+        if table_data[2] >= 80.00:
+            score_style = "bold bright_green"
+        elif table_data[2] > 0.00:
+            score_style = "bold bright_yellow"
+        else:
+            score_style = "bold bright_red"
+            
+        table.add_row(table_data[0],table_data[1],f"[{score_style}]{str(table_data[2])}[/{score_style}]", table_data[3])
+        Console.print(table)
 
     def exit_app(self):
         Console.print("Exiting the app...", style="bold red")
@@ -164,11 +202,9 @@ class CommandUI:
 
     def run(self):
         while True:
-            # Show the prompt
-            command = Prompt.ask("\nEnter a command", default="help")
+            command = Prompt.ask("\nEnter a command", default="help") # Show the prompt
             if command in self.commands:
-                # Run the command if valid
-                self.commands[command]()
+                self.commands[command]()# Run the command if valid
             else:
                 # Handle invalid command
                 Console.print(f"[bold red]Invalid command:[/bold red] {command}. Type [bold green]help[/bold green] for available commands.")
